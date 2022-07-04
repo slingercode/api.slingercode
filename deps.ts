e1fcc4c0cf8d6ce17f1f8c355cc872c692be0b9c
Noel Espino Córdova <noelespinocordova@tutanota.com>
2022-07-03 23:07:19 -0500

    Fix dotenv library

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 import * as std from "https://deno.land/std@0.146.0/http/mod.ts";
-import * as dotenv from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import * as dotenv from "https://deno.land/std@0.146.0/dotenv/mod.ts";
 
 export { std, dotenv };
